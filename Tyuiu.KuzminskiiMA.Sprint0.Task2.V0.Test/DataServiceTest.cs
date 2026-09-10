08f212ef02298d720ea014b59ec7a9150debc8c7
kuzminskiiMaxim <immmpulsee@yandex.ru>
2026-09-10 21:38:48 +0500

    Task2

---
+++ Tyuiu.KuzminskiiMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KuzminskiiMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KuzminskiiMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет Максим", res);
+        }
+    }
+}
