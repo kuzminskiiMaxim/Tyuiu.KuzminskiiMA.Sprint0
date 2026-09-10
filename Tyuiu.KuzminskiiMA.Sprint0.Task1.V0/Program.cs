c7f3af5bf17a284241ea0638fd33998848c94289
kuzminskiiMaxim <immmpulsee@yandex.ru>
2026-09-10 20:40:33 +0500

    task 1

---
+++ Tyuiu.KuzminskiiMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KuzminskiiMA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Максим";
+            int age = 20;
+            Console.WriteLine("Здраствуsssйте Меня зовут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
