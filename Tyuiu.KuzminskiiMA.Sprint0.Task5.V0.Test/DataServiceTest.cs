eebcd4a8eb3db3566a979713946429c7609ea209
kuzminskiiMaxim <immmpulsee@yandex.ru>
2026-09-10 23:00:56 +0500

    Task5

---
+++ Tyuiu.KuzminskiiMA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KuzminskiiMA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KuzminskiiMA.Sprint0.Task5.V0.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
+
