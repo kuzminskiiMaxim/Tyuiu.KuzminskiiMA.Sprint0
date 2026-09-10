40e136dc27a07b48f74d2b8a73431d1dbd40bd5f
kuzminskiiMaxim <immmpulsee@yandex.ru>
2026-09-10 22:17:07 +0500

    Task4

---
+++ Tyuiu.KuzminskiiMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.KuzminskiiMA.Sprint0.Task4.V0.Lib;
+
+static void Main(string[] args)
+{
+    Console.WriteLine(DataService.Addition(1, 5));
+    Console.WriteLine(DataService.Subtraction(15, 5));
+    Console.WriteLine(DataService.Multiplication(10, 10));
+    Console.WriteLine(DataService.Division(5, 5));
+    Console.ReadKey();
+}
